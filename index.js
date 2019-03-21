dfea2359d60d279dfcda9c6de19c0bbdc720fe54
WaleedAshraf <waleedashraf@outlook.com>
2019-03-21 15:50:06 +0100

    eslint recommended

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 module.exports = {
-  extends: ['google', 'plugin:node/recommended'],
+  extends: ['google', 'plugin:node/recommended', 'eslint:recommended'],
   env: {
     node: true,
     mocha: true,
     es6: true,
   },
   rules: {
-    camelcase: ['warn'],
-    'prefer-const': ['error'],
-    'no-implicit-coercion': [0],
-    semi: [2, 'never'],
-    'no-multiple-empty-lines': [0],
+    // off
+    'no-implicit-coercion': 'off',
+    'no-multiple-empty-lines': 'off',
+    'node/no-unpublished-import': 'off',
+    'node/no-unpublished-require': 'off',
+
+    // warn
+    'camelcase': 'warn',
+    'comma-dangle': ['warn', 'always-multiline'],
+    'no-process-exit': 'warn',
+    'node/no-deprecated-api': 'warn',
+    'node/no-extraneous-require': 'warn',
+    'node/no-missing-require': 'warn',
+    'node/no-unsupported-features/es-builtins': 'warn',
+    'node/no-unsupported-features/es-syntax': 'warn',
+    'node/no-unsupported-features/node-builtins': 'warn',
+    'node/process-exit-as-throw': 'warn',
+    'node/shebang': 'warn',
+    'node/no-unpublished-bin': 'warn',
+
+    // error
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'semi': ['error', 'never'],
+    'max-len': ['error', 140, 4],
+    'generator-star-spacing': ['error', {before: true, after: false}],
+    'quotes': ['error', 'single'],
+    'strict': ['error', 'global'],
     'new-cap': [
-      2,
+      'error',
       {
-        capIsNewExceptions: [
+        'capIsNewExceptions': [
           'DataTypes.ARRAY',
           'DataTypes.STRING',
           'Sequelize.ARRAY',
@@ -25,23 +48,6 @@ module.exports = {
         ],
       },
     ],
-    'max-len': [2, 140, 4],
-    'no-var': [2],
-    'generator-star-spacing': [2, {before: true, after: false}],
-    quotes: [2, 'single'],
-    strict: ['error', 'global'],
-    'comma-dangle': ['warn', 'always-multiline'],
-    "no-process-exit": "warn",
-    "node/no-deprecated-api": "warn",
-    "node/no-extraneous-require": "warn",
-    "node/no-missing-require": "warn",
-    "node/no-unpublished-bin": "warn",
-    "node/no-unpublished-require": "warn",
-    "node/no-unsupported-features/es-builtins": "warn",
-    "node/no-unsupported-features/es-syntax": "warn",
-    "node/no-unsupported-features/node-builtins": "warn",
-    "node/process-exit-as-throw": "warn",
-    "node/shebang": "error"
   },
   parserOptions: {
     ecmaVersion: 2018,
